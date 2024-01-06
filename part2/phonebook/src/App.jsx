97013cd56279ccb7961eb7e362ea0d1883e5bf94
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2024-01-06 23:29:43 +0200

    Finish 2.8 exercise.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: "201613275978"}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('');
 
   const isPersonExistBefore = (str, arr)=>
   {
@@ -26,9 +27,10 @@ const App = () => {
     }
     else
     {
-      setPersons(persons.concat({name: newName}));
+      setPersons(persons.concat({name: newName, number: newNumber}));
     }
     setNewName("");
+    setNewNumber("");
   }
   return (
     <div>
@@ -37,13 +39,14 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={(e)=> setNewName(e.target.value)}/>
         </div>
-        <div>debug: {newName}</div>
+        <div>number: <input value={newNumber} onChange={(e)=> setNewNumber(e.target.value)}/></div>
+        <div>debug: {newNumber}</div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person)=>(<p key={person.name}>{person.name}</p>))}
+      {persons.map((person)=>(<p key={person.name}>{person.name} {person.number}</p>))}
     </div>
   )
 }
