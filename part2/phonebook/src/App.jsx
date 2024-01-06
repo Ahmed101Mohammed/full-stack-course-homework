7703f5c25c18e1676bddcd6411a4f4feb50b99f4
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2024-01-07 00:04:43 +0200

    Finish exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,42 @@
 import { useState } from 'react'
 
+const Filter = ({search, handleChange})=>
+{
+  return (
+    <form>
+        <label htmlFor="search">search:</label>
+        <input type="search" id="search" value={search} onChange={handleChange} />
+    </form>
+  )
+}
+
+const PersonForm = ({handleSubmit, newName, newNumber, handleChangeForName, handleChangeForNumber})=>
+{
+  return(
+    <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChangeForName}/>
+        </div>
+        <div>number: <input value={newNumber} onChange={handleChangeForNumber}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+    </form>
+  )
+}
+
+const Person = ({person})=>{
+  return <p>{person.name} {person.number}</p>
+}
+
+const Persons = ({persons})=>
+{
+  return(
+    <>
+      {persons.map((person)=>(<Person key={person.name} person={person}/>))}
+    </>
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -42,26 +79,20 @@ const App = () => {
   {
     setSearch(e.target.value);
   }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <label htmlFor="search">search:</label>
-        <input type="search" id="search" value={search} onChange={searching} />
-      </form>
-
-      <form onSubmit={sumit}>
-        <div>
-          name: <input value={newName} onChange={(e)=> setNewName(e.target.value)}/>
-        </div>
-        <div>number: <input value={newNumber} onChange={(e)=> setNewNumber(e.target.value)}/></div>
-        <div>debug: {newNumber}</div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter search={search} handleChange={searching}/>
+      <PersonForm newName={newName} 
+                  newNumber={newNumber} 
+                  handleSubmit={sumit} 
+                  handleChangeForName={(e)=> setNewName(e.target.value)} 
+                  handleChangeForNumber={(e)=> setNewNumber(e.target.value)}/>
+      
       <h2>Numbers</h2>
-      {newPersons.map((person)=>(<p key={person.name}>{person.name} {person.number}</p>))}
+      <Persons persons={newPersons}/>
+      
     </div>
   )
 }
