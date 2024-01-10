91ffc2a48e5c2078cba7a11a6f80d5c55491db5a
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2024-01-10 13:51:36 +0200

    Finish 2.11 exercise.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({search, handleChange})=>
 {
@@ -38,12 +39,7 @@ const Persons = ({persons})=>
   )
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState("");
@@ -72,7 +68,10 @@ const App = () => {
     setNewName("");
     setNewNumber("");
   }
-
+  useEffect(()=>{
+    const promise = axios.get('http://localhost:3001/persons') 
+    promise.then(r=>setPersons(r.data))
+  }, [])
   const newPersons = persons.filter((person)=> person.name.slice(0, search.length).toLowerCase() === search.toLowerCase());
   
   const searching = (e)=>
